2e9296d4e472228aab4336e3028338d375bf1b3f
junmov <junmov@outlook.com>
2020-11-21 09:43:59 +0800

    feat:导航到添加账户页面

--- app/src/main/java/cn/junmov/mirror/account/ui/AccountFragment.kt
+++ app/src/main/java/cn/junmov/mirror/account/ui/AccountFragment.kt
@@ -1,11 +1,11 @@
 package cn.junmov.mirror.account.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import cn.junmov.mirror.R
 import cn.junmov.mirror.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
@@ -16,6 +16,24 @@ class AccountFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentAccountBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_create, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.option_create -> {
+                findNavController().navigate(
+                    AccountFragmentDirections.actionPageAccountToAccountFormDialog()
+                )
+                true
+            }
+            else -> false
+        }
+    }
+
 }
