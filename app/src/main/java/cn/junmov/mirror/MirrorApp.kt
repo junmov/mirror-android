5ef13afad9d6cce28eb20fe023490fdfd7482b67
junmov <junmov@outlook.com>
2020-11-21 08:49:15 +0800

    feat: core模块

---
+++ app/src/main/java/cn/junmov/mirror/MirrorApp.kt
@@ -0,0 +1,8 @@
+package cn.junmov.mirror
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MirrorApp : Application() {
+}
---
+++ app/src/main/java/cn/junmov/mirror/core/CoreModule.kt
@@ -0,0 +1,26 @@
+package cn.junmov.mirror.core
+
+import android.content.Context
+import androidx.room.Room
+import cn.junmov.mirror.core.data.MirrorDatabase
+import cn.junmov.mirror.core.data.Scheme
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object CoreModule {
+
+    @Singleton
+    @Provides
+    fun provideDataBase(@ApplicationContext context: Context): MirrorDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext, MirrorDatabase::class.java, Scheme.DATABASE_NAME
+        ).build()
+    }
+
+}
