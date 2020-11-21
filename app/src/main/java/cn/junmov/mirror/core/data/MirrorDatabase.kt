0bb67a3bb6168804dee0a2a5788ab102ef76742d
junmov <junmov@outlook.com>
2020-11-21 08:53:41 +0800

    feat:在数据库中注册dao

---
+++ app/src/main/java/cn/junmov/mirror/core/data/MirrorDatabase.kt
@@ -0,0 +1,24 @@
+package cn.junmov.mirror.core.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import cn.junmov.mirror.core.data.dao.*
+import cn.junmov.mirror.core.data.entity.*
+
+@Database(
+    entities = [
+        Account::class, Budget::class, Split::class, Thing::class, Trade::class, Voucher::class
+    ],
+    version = Scheme.DATABASE_VERSION,
+    exportSchema = false,
+)
+@TypeConverters(DataTypeConverters::class)
+abstract class MirrorDatabase : RoomDatabase() {
+    abstract fun accountDao(): AccountDao
+    abstract fun budgetDao(): BudgetDao
+    abstract fun thingDao(): ThingDao
+    abstract fun voucherDao(): VoucherDao
+    abstract fun tradeDao(): TradeDao
+
+}
