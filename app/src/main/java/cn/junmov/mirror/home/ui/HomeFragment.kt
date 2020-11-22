bc19c99b7ff1c527b1c0abb39696294c89131a8c
junmov <junmov@outlook.com>
2020-11-22 10:19:53 +0800

    fix:修复不能导航到项目列表的问题

--- app/src/main/java/cn/junmov/mirror/home/ui/HomeFragment.kt
+++ app/src/main/java/cn/junmov/mirror/home/ui/HomeFragment.kt
@@ -1,11 +1,12 @@
 package cn.junmov.mirror.home.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import cn.junmov.mirror.R
 import cn.junmov.mirror.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -16,6 +17,17 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_home, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController()
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
 }
