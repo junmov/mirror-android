c4da6407cbc90dd949f7d8d56963f61c96d3ce97
junmov <junmov@outlook.com>
2021-01-14 21:31:29 +0800

    fix:修复资产详情显示的问题

--- app/src/main/java/cn/junmov/mirror/asset/ui/AssetDetailViewModel.kt
+++ app/src/main/java/cn/junmov/mirror/asset/ui/AssetDetailViewModel.kt
@@ -6,6 +6,7 @@ import cn.junmov.mirror.asset.domain.FlowAllAssetLogUseCase
 import cn.junmov.mirror.asset.domain.FlowAssetUseCase
 import cn.junmov.mirror.core.data.db.entity.Asset
 import cn.junmov.mirror.core.data.db.entity.AssetLog
+import cn.junmov.mirror.core.utility.MoneyUtils
 
 class AssetDetailViewModel @ViewModelInject constructor(
     private val flowAsset: FlowAssetUseCase,
@@ -18,6 +19,12 @@ class AssetDetailViewModel @ViewModelInject constructor(
 
     val assetLogs: LiveData<List<AssetLog>> =
         _assetId.switchMap { flowAllAssetLog(it).asLiveData() }
+
+    val count: LiveData<String> = asset.map { "持有数量： ${MoneyUtils.centToYuan(it.count)}" }
+
+    val cost: LiveData<String> = asset.map { "总投入成本： ${MoneyUtils.centToYuan(it.buy)}" }
+
+    val earning: LiveData<String> = asset.map { "已获得回报： ${MoneyUtils.centToYuan(it.sell)}" }
 
     fun loadData(assetId: Long) {
         _assetId.value = assetId
